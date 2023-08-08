3a6858e93ea2e04d62a3f62696cf5817580f5b27
Rahul kapoor <kapoorrahul304@gmail.com>
2023-08-08 12:00:52 +0530

    assign complete

--- block-BRaadi/code/src/components/App.js
+++ block-BRaadi/code/src/components/App.js
@@ -3,6 +3,12 @@ import Header from "./Header";
 import Main from "./Main";
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+   this.state ={
+      isDarkMode:false,
+    }
+  }
   render() {
     let { isDarkMode } = this.state;
     return (
